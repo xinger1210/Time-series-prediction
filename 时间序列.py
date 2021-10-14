d65345026d479c602a23a4e37dd45db253e573c3
zx <1103504204@qq.com>
2021-10-14 15:40:59 +0800

    add

--- 时间序列.py
+++ 时间序列.py
@@ -6,6 +6,7 @@ df = pd.read_csv('train.csv')
 # print(df.head())
 trian=df[0:10392]
 test=df[10392:]
+# print(test)
 df["Timestamp"]=pd.to_datetime(df["Datetime"],format="%d-%m-%Y %H:%M")
 
 df.index=df["Timestamp"]
@@ -16,24 +17,46 @@ df=df.resample("D").mean()
 trian["Timestamp"]=pd.to_datetime(trian["Datetime"],format="%d-%m-%Y %H:%M")
 
 trian.index=trian["Timestamp"]
-print(trian.head())
+# print(trian.head())
 trian=trian.resample("D").mean()
-print(trian.head())
+# print(trian.head())
 
 test['Timestamp'] = pd.to_datetime(test['Datetime'], format='%d-%m-%Y %H:%M')
 test.index = test['Timestamp']
-print(test.head())
+# print(test.head())
 test = test.resample('D').mean()
-print(test.head())
+# print(test.head())
 
-trian.Count.plot(figsize=(15,8), title= 'Daily Ridership', fontsize=14)
-test.Count.plot(figsize=(15,8), title= 'Daily Ridership', fontsize=14)
-plt.show()
+# trian.Count.plot(figsize=(15,8), title= 'Daily Ridership', fontsize=14)
+# test.Count.plot(figsize=(15,8), title= 'Daily Ridership', fontsize=14)
+# plt.show()
 
 #朴素法预测
 #如果数据集在一段时间内都很稳定，我们想预测第二天的价格，
 # 可以取前面一天的价格，预测第二天的值。
 # 这种假设第一个预测点和上一个观察点相等的预测方法就叫朴素法
+dd=np.asarray(trian["Count"])
+y_hat=test.copy()
+y_hat["native"]=dd[len(dd)-1]
+# print(y_hat)
+plt.figure(figsize=(12, 8))
+
+# print(y_hat.index)
+# plt.plot(trian.index,trian["Count"], label='Train')
+# plt.plot(test.index, test['Count'], label='Test')
+# plt.plot(y_hat.index, y_hat['native'], label='Naive Forecast')
+# plt.legend(loc='best')
+# plt.title("Naive Forecast")
+# plt.show()
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+rms=sqrt(mean_squared_error(test["Count"],y_hat["native"]))
+print(rms)
+
+#简单平均法
+
+
+
 
 
 
